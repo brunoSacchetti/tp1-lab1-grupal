e383d29c0955b9e512e2f2f68e550dc919478e1d
MaxiColombo <maximocolombo2003@gmail.com>
2024-03-25 17:26:06 -0300

    Installs para todos

---
+++ app.js
@@ -0,0 +1,39 @@
+/* INICIALIZAR PROYECTO */
+/* NPM INIT -Y 
+NPM INSTALL EXPRESS
+NPM INSTALL NODEMON
+NPM I MYSQL
+NPM INSTALL PATH 
+
+*/
+
+const express = require('express'); /* <- ruteo backend */
+
+const path = require("path");
+
+const app = express();
+app.use(express.urlencoded({
+  extended: false
+})); //Esto es para formData
+app.use(express.json())
+
+
+/* SISTEMA DE RUTEOS */
+app.get("/", (req, res) => {
+  res.json("funciona") /* CREAR UN HOME */
+})
+
+/* ESTO ES PARA CARGAR VISTAS */
+app.get("/empresas", (req, res) => {
+  res.render("administrarEmpresa.ejs");
+});
+
+app.get("/noticias", (req, res) => {
+  res.render("administrarNoticia.ejs")
+})
+
+
+const puerto = 3300;
+app.listen(puerto, () => {
+  console.log(`Servidor en ejecución en http://localhost:${puerto}`);
+});
